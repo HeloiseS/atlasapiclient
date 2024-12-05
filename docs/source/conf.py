f02cbb2682896f9c870e092336e5c2eefb10c642
Jack Leland <jack.leland@dtc.ox.ac.uk>
2024-12-05 11:14:34 +0000

    Add first attempt at barebones sphinx docs + build workflow

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ATLAS API Client'
+copyright = '2024, Heloise Stevance'
+author = 'Heloise Stevance'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+from atlasapiclient import __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
