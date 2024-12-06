40f96b2324a7586d7eae605d534eefbf660bbfcd
Heloise Stevance <hfstevance@gmail.com>
2024-12-06 08:57:30 +0000

    BUG FIX

--- atlasapiclient/__init__.py
+++ atlasapiclient/__init__.py
@@ -1,6 +1,6 @@
 #import importlib.metadata
 
-#from . import client, exceptions, utils
+from . import client, exceptions, utils
 
 
 #__version__ = importlib.metadata.version(__package__ or __name__)
