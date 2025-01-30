8daa1fe273238eea4fc907c89303f290269953c0
Jack Leland <jack.leland@dtc.ox.ac.uk>
2025-01-30 14:49:07 +0000

    Add url parsing function + tests

--- atlasapiclient/utils.py
+++ atlasapiclient/utils.py
@@ -1,5 +1,8 @@
 import pkg_resources
 import os
+import urllib
+
+from .exceptions import ATLASAPIClientError 
 
 # Useful Constants
 LIST_NAMES = "follow_up, good, possible, eyeball, attic, cv, mdwarf, pm_stars"
@@ -27,3 +30,27 @@ dict_list_id = {'garbage': [0, False],
                 'vra': [73, True],
                 'dummy': [999, True]
                 }
+
+            
+def get_url(config_val: str) -> str:
+    """Ensure that the URL is valid, using urllib, and return it"""
+    # Parse the URL into its components
+    try:
+        url_parts = urllib.parse.urlparse(config_val)
+    except Exception as e:
+        raise ATLASAPIClientError(f"Invalid URL - exception caused: {config_val}, {e}")
+    
+    # Do some validation
+    if not url_parts.scheme or not url_parts.netloc:
+        raise ATLASAPIClientError(f"Invalid URL: {config_val}")
+    elif url_parts.scheme not in ['http', 'https']:
+        raise ATLASAPIClientError(f"Invalid URL scheme: {url_parts.scheme}")
+    
+    # Add a trailing slash to the path
+    if url_parts.path:
+        if url_parts.path[-1] != '/':
+            url_parts = url_parts._replace(path=url_parts.path + '/')
+    else:
+        url_parts = url_parts._replace(path='/')
+    
+    return urllib.parse.urlunparse(url_parts)
